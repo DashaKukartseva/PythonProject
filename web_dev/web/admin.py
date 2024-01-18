6ae6a6cd2e221c450fe954320b7b22870cee0b36
DashaKukartseva <d.kakartseva@gmail.com>
2024-01-18 13:01:16 +0500

    register models

--- web_dev/web/admin.py
+++ web_dev/web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Navigation)
+admin.site.register(MainPage)
+admin.site.register(Relevance)
+admin.site.register(Location)
+admin.site.register(Abilities)
+admin.site.register(HeadHunterLV)
